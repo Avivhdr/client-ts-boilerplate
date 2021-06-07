7fd224d2a7c1f2a959b0cb153379fd12741d31ec
Aviv Hadar <avivhdr@gmail.com>
2021-06-07 15:04:07 +0200

    x

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React
+          Learn React with style
         </a>
       </header>
     </div>
